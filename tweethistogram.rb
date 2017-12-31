0edfb2444fc40a9e019d0179ccad23067cf99e52
redxlegion <redlegion@gmail.com>
2017-12-31 13:46:50 -0500

    Added tweet histogram

---
+++ tweethistogram.rb
@@ -0,0 +1,16 @@
+require 'csv'
+require 'time'
+require 'tzinfo'
+
+fil = ARGV[0]
+stuff = Array.new
+
+tweets = CSV.read(fil)
+tweets.each do |x|
+	if x[1] != "created_at" and x[3].start_with?("RT @") == false then
+		times = Time.parse(x[1].to_s)
+		shift = times.getlocal("-05:00")
+		stuff << "#{shift.hour}:#{shift.min}:#{shift.sec}"
+	end
+end
+puts stuff
