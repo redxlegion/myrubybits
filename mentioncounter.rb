7340380115e5b6adff3793d2fe94685463c61283
redxlegion <redlegion@gmail.com>
2018-01-07 15:15:43 -0500

    Formatting

--- mentioncounter.rb
+++ mentioncounter.rb
@@ -7,6 +7,6 @@ tweets = CSV.read(fil+'_tweets.csv')
 tweets = tweets.to_s.scan(/@\w+/)
 counts = Hash.new 0
 tweets.each { |word| counts[word] += 1 }
-lesser = counts.select { |k,v| v > num }.sort_by { |k,v| v }.reverse
-lesser = lesser.reject { |k,v| k =~ /#{fil}/ }
-lesser.each { |a,b| print "@#{fil}, ",a,', ',b,"\n" }
+lesser = counts.select { |k, v| v > num }.sort_by { |_k, v| v }.reverse
+lesser = lesser.reject { |k, _v| k =~ /#{fil}/ }
+lesser.each { |a, b| print "@#{fil}, ", a, ', ', b, "\n" }
