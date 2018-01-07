22c3d38438eb648bfb82744d5fedf28d01e66580
redxlegion <redlegion@gmail.com>
2018-01-07 15:11:13 -0500

    Quick updates

--- tweet_scraper.rb
+++ tweet_scraper.rb
@@ -6,15 +6,15 @@ require 'csv'
 require 'progressbar'
 
 client = Twitter::REST::Client.new do |config|
-	config.consumer_key = "YoursHere"
-	config.consumer_secret = "YoursHere"
-	config.access_token = "YoursHere"
-	config.access_token_secret = "YoursHere"
+  config.consumer_key = 'YoursHere'
+  config.consumer_secret = 'YoursHere'
+  config.access_token = 'YoursHere'
+  config.access_token_secret = 'YoursHere'
 end
 
 scrname = String.new ARGV[0]
 
-def collect_with_max_id(collection=[], max_id=nil, &block)
+def collect_with_max_id(collection = [], max_id = nil, &block)
   response = yield(max_id)
   collection += response
   response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
@@ -23,9 +23,9 @@ end
 def client.get_all_tweets(user)
   twtcount = user(user).statuses_count
   if twtcount > 3200
-      twtcount = 3200 / 200
+    twtcount = 3200 / 200
   else
-      twtcount = twtcount / 200
+    twtcount /= 200
   end
   pbar = ProgressBar.new("Downloading", twtcount)
   collect_with_max_id do |max_id|
@@ -39,7 +39,7 @@ end
 junk = client.get_all_tweets(scrname)
 
 CSV.open("#{scrname}.csv", "w") do |csv|
-	junk.each do |tweet|
-		csv << [tweet.id, tweet.created_at, tweet.user.screen_name, tweet.text, tweet.source, tweet.geo]
-	end
+  junk.each do |tweet|
+    csv << [tweet.id, tweet.created_at, tweet.user.screen_name, tweet.text, tweet.source, tweet.geo]
+  end
 end
--- tweethistogram.rb
+++ tweethistogram.rb
@@ -3,14 +3,14 @@ require 'time'
 require 'tzinfo'
 
 fil = ARGV[0]
-stuff = Array.new
+stuff = Array []
 
 tweets = CSV.read(fil)
 tweets.each do |x|
-	if x[1] != "created_at" and x[3].start_with?("RT @") == false then
-		times = Time.parse(x[1].to_s)
-		shift = times.getlocal("-05:00")
-		stuff << "#{shift.hour}:#{shift.min}:#{shift.sec}"
-	end
+  if x[1] != 'created_at' and x[3].start_with?('RT @') == false then
+    times = Time.parse(x[1].to_s)
+    shift = times.getlocal('-05:00')
+    stuff << "#{shift.hour}:#{shift.min}:#{shift.sec}"
+  end
 end
 puts stuff
